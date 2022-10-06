be72e2b6387cea499e6d1505714926ee63064753
harsh <93788928+harsh23079@users.noreply.github.com>
2022-10-06 09:50:54 +0530

    Update C++.cc
    i am new to open source don't know how my request is been counted  for hactoberfest 2022

--- C++/C++.cc
+++ C++/C++.cc
@@ -16,5 +16,46 @@ int main()
         }
         cout << "\n";
     }
+    
+    
+        cout<<"\nPattern 10 :- Pyramid Patter with having numbers like an example \n"<<endl;
+/*
+   1
+  212
+ 32123
+4321234
+*/
+
+   int n=rows;
+    
+
+       for(int i=1;i<=n;i++)
+      {
+        int j=1;
+        int p=i;
+        int l=2;
+        while(j<=n+i-1)
+        {
+            if(j<=n-i)
+            {
+               cout<<" ";
+
+            }
+            else if(j>n-i && j<=n)
+            {
+               cout<<p;
+                p--;
+            }
+            else 
+            {
+                cout<<l;
+                l++;
+            }
+            j++;
+         }
+        cout<<endl;
+      }
+    
+    
     return 0;
 }
